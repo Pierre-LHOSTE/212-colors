c02c655533facc2be90de0da0ce9a90f62077ce7
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-07-11 10:56:06 +0200

    Update NavHeader

--- src/components/navAside/navHeader/NavHeader.tsx
+++ src/components/navAside/navHeader/NavHeader.tsx
@@ -1,14 +1,22 @@
 import { useDataStore } from "@/src/store/data";
 import "./nav-header.scss";
+import { usePathname } from "next/navigation";
 
 export default function NavHeader() {
+  const pathname = usePathname();
   const project = useDataStore((state) => state.project);
 
   return (
     <header id="nav-header">
       <div id="nav-header-wrapper">
         <div>
-          <span>{project.name}</span>
+          <span>
+            {pathname.endsWith("profile")
+              ? "Profile"
+              : pathname.endsWith("settings")
+                ? "Settings"
+                : project.name}
+          </span>
         </div>
       </div>
     </header>
