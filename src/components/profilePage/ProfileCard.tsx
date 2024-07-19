d7e20771be5a022f69c8b6b7d37aa063e219dbf0
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-07-20 00:48:28 +0200

    Add no avatar case

--- src/components/profilePage/ProfileCard.tsx
+++ src/components/profilePage/ProfileCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useI18nContext } from "@/src/i18n/i18n-react";
 import type { UserType } from "@/src/types/user";
+import { IconX } from "@tabler/icons-react";
 import { Image } from "antd";
 import MainCard from "../card/MainCard";
 import "./profile.scss";
@@ -19,7 +20,11 @@ export default function ProfileCard({ user }: { user: UserType }) {
           },
           {
             title: LL.profile.display.avatar(),
-            children: <Image src={user.image} alt={user.name} />,
+            children: user.image ? (
+              <Image src={user.image} alt={user.name} />
+            ) : (
+              <IconX size={24} />
+            ),
           },
           {
             title: LL.profile.display.email(),
