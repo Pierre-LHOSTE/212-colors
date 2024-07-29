7171889044c46bba98de3d21d757d772e0f2fd60
Pierre LHOSTE <vingt-douze@protonmail.com>
2024-07-30 00:24:38 +0200

    Add store & lib func

---
+++ src/lib/generatePrimaryColor.ts
@@ -0,0 +1,14 @@
+import { ColorType } from "../types/color";
+
+function generatePrimaryColor(colors: ColorType[]) {
+  const firstPrimaryColor = colors.find((color) => color.type === "primary");
+  if (firstPrimaryColor) return firstPrimaryColor.color;
+
+  const firstSecondaryColor = colors.find(
+    (color) => color.type === "secondary"
+  );
+  if (firstSecondaryColor) return firstSecondaryColor.color;
+
+  const firstSpecialColor = colors.find((color) => color.type === "special");
+  if (firstSpecialColor) return firstSpecialColor.color;
+}
